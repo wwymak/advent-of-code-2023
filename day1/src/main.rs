e76e7f5cf5a79f301129fafdf199b20eeb185680
wwymak <6398157+wwymak@users.noreply.github.com>
2023-12-03 14:55:15 +0000

    attempting section2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -17,13 +17,30 @@ fn number_extractor(text: &str) -> i32 {
     return output;
 
 }
+
+fn number_extractor_v2(text: &str) -> i32{
+    let mut output: i32 = 0;
+
+    let valid_number_str: Vec<&str> = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let mut first_item = -1;
+    let mut last_item = -1;
+    let mut first_index = -1;
+    let mut last_index = -1;
+
+    for val in valid_number_str.iter() {
+        let v: Vec<_> = text.match_indices(val).collect();
+
+    }
+
+    return output
+
+}
 fn main() {
     let mut sum = 0;
     if let Ok(lines) = read_lines("/home/wwymak/code_experiments/advent_of_code_2023/day1/src/input1.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
-                // let is_numeric = ip.chars().map(|x| x.is_digit(10));
 
                 sum += number_extractor(&ip);
                 
